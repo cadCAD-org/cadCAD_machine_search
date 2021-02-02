1bf8289581955088a5b66b534c157aa76606a666
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-02-02 20:40:44 -0300

    fix

--- cadcad_machine_search/tools/param_sweep.py
+++ cadcad_machine_search/tools/param_sweep.py
@@ -16,10 +16,11 @@ def sweep_cartesian_product(sweep_params: Dict[str, tuple]) -> Dict[str, tuple]:
     Usage:
     >>> sweep_params = {'a': [0.1, 0.2], 'b': [1, 2]}
     >>> cartesian_product_sweep(sweep_params)
-    {'a': (0.1, 0.1, 0.2, 0.2), 'b': (1, 2, 1, 2)}
+    {'a': [0.1, 0.1, 0.2, 0.2], 'b': [1, 2, 1, 2]}
     """
     cartesian_product = product(*sweep_params.values())
     transpose_cartesian_product = zip(*cartesian_product)
     zipped_sweep_params = zip(sweep_params.keys(), transpose_cartesian_product)
     sweep_dict = dict(zipped_sweep_params)
+    sweep_dict = {k: list(v) for k, v in sweep_dict.items()}
     return sweep_dict
